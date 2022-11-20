66f1a07db7f2783d7fdba87b30f7d089c497f988
carlos8a <caanochoame@ittepic.edu.mx>
2022-11-20 14:14:47 -0600

    Primer Commit

---
+++ src/components/Foter.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from 'react-bootstrap/foter';
+
+export function Fotter()  {
+    return (
+        <div className='conteiner'>
+            <div className='row'>
+                <div className='col1'>
+                    <h3>Prueba</h3>
+                    <ul></ul>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+export default Fotter;
