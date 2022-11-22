c6b4e4866441e09df9a23d36e02539132fc57e6a
carlos8a <caanochoame@ittepic.edu.mx>
2022-11-21 18:44:14 -0600

    segundo commit 21/11

--- src/components/Foter.jsx
+++ src/components/Foter.jsx
@@ -1,17 +1,67 @@
-import React from 'react'
-import Footer from 'react-bootstrap/foter';
+import React from 'react';
+import { CDBFooter, CDBFooterLink, CDBBox, CDBBtn, CDBIcon } from 'cdbreact';
 
-export function Fotter()  {
-    return (
-        <div className='conteiner'>
-            <div className='row'>
-                <div className='col1'>
-                    <h3>Prueba</h3>
-                    <ul></ul>
-                </div>
-            </div>
 
-        </div>
-    );
-}
-export default Fotter;
+export const Foter = () => {
+  return (
+    <CDBFooter className="shadow">
+      <CDBBox display="flex" flex="column" className="mx-auto py-5" style={{ width: '90%' }}>
+        <CDBBox display="flex" justifyContent="between" className="flex-wrap">
+          <CDBBox>
+            <a href="/" className="d-flex align-items-center p-0 text-dark">
+              
+              <span className="ml-3 h5 font-weight-bold">⚡Tormenta⚡</span>
+            </a>
+            <p className="my-3" style={{ width: '250px' }}>
+              Nuestro Objetivo siempre sera tener todo lo que buscas
+              ¡Tormenta!
+            </p>
+            <CDBBox display="flex" className="mt-4">
+              <CDBBtn flat color="dark">
+                <CDBIcon fab icon="facebook-f" />
+              </CDBBtn>
+              <CDBBtn flat color="dark" className="mx-3">
+                <CDBIcon fab icon="twitter" />
+              </CDBBtn>
+              <CDBBtn flat color="dark" className="p-2">
+                <CDBIcon fab icon="instagram" />
+              </CDBBtn>
+            </CDBBox>
+          </CDBBox>
+          <CDBBox>
+            <p className="h5 mb-4" style={{ fontWeight: '600' }}>
+              Ayuda
+            </p>
+            <CDBBox flex="column" style={{ cursor: 'pointer', padding: '0' }}>
+              <CDBFooterLink href="/">Resources</CDBFooterLink>
+              <CDBFooterLink href="/">Acerca de nosotros</CDBFooterLink>
+              <CDBFooterLink href="/">Contacto</CDBFooterLink>
+              <CDBFooterLink href="/">Soporte</CDBFooterLink>
+            </CDBBox>
+          </CDBBox>
+          <CDBBox>
+            <p className="h5 mb-4" style={{ fontWeight: '600' }}>
+              Ayuda
+            </p>
+            <CDBBox flex="column" style={{ cursor: 'pointer', padding: '0' }}>
+              <CDBFooterLink href="/">Soporte</CDBFooterLink>
+              <CDBFooterLink href="/">Iniciar Secion</CDBFooterLink>
+              <CDBFooterLink href="/">Ingresar</CDBFooterLink>
+            </CDBBox>
+          </CDBBox>
+          <CDBBox>
+            <p className="h5 mb-4" style={{ fontWeight: '600' }}>
+              Products
+            </p>
+            <CDBBox flex="column" style={{ cursor: 'pointer', padding: '0' }}>
+              <CDBFooterLink href="/">Windframe</CDBFooterLink>
+              <CDBFooterLink href="/">Loop</CDBFooterLink>
+              <CDBFooterLink href="/">Contrast</CDBFooterLink>
+            </CDBBox>
+          </CDBBox>
+        </CDBBox>
+        <small className="text-center mt-5">&copy; Todos los derechos reservados.</small>
+      </CDBBox>
+    </CDBFooter>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './'
+import { CDBFooter, CDBFooterLink, CDBBox, CDBBtn, CDBIcon } from 'cdbreact';
+
+
 import { BarraN } from "./components/NavBar";
+import { Foter } from "./components/Foter";
+
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<BarraN/>, document.getElementById("root"));
+
+
+
+ReactDOM.render(<BarraN/>, document.getElementById('root'));
+ReactDOM.render(<Foter/>, document.getElementById('root'));
+
+
